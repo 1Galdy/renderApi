a319f027b07d09e33eeb392cd7543d779b2d2e0c
1Galdy <117950402+1Galdy@users.noreply.github.com>
2025-04-25 02:01:57 +0200

    vérification du DC déploiement continu

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World depuis mon API Node en ligne !');
+  res.send('Hello World depuis mon API Node en ligne ! Tu as enfin réussi à la mettre en ligne Brecht');
 });
 
 app.listen(PORT, '0.0.0.0', () => {
